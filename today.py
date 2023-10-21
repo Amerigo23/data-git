a39064402ae624d199c5d39100a4827295cc1f1b
Amer Mujkanovic <mujkanovic.amer@gmail.com>
2023-10-21 15:30:00 +0100

    Fix style issues, should get a 'Good Style' now 🙏

--- today.py
+++ today.py
@@ -3,7 +3,9 @@ This is the module docstring
 '''
 
 def my_name_is():
+    """returns my name"""
     return "Amerigo23"
 def my_buddy_is():
+    """returns my buddy's name"""
     return "nruth"
 pass  # YOUR CODE HERE
